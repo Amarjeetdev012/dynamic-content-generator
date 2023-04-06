40c63d659b27f461077d8815cc717bb2bf062990
unknown <amarjeet@baruzotech.com>
2023-04-06 10:09:24 +0530

    update tag function
    update tag function where we can use @ or # or both

--- controller/content.controller.js
+++ controller/content.controller.js
@@ -23,10 +23,19 @@ export const test = (req, res) => {
   try {
     const data = req.body;
     console.log('data', data);
-    const str = data.mix.replace(
-      /{{{"value":"([^"]+)","prefix":"[@#]"}}}/g,
-      '$1'
-    );
+    function filterText(input) {
+      const str = data.mix;
+      let filteredStr = str.replace(
+        /{{{"value":"([^"]+)","prefix":"[#@]+"}}}/g,
+        '$1'
+      );
+      filteredStr = filteredStr.replace(
+        /\[\[{2}"value":"([^"]+)","prefix":"[#@]+"\]{2}/g,
+        '$1'
+      );
+      return filteredStr.trim();
+    }
+    const str = filterText(data);
     console.log('str', str);
     const inputText = req.body.text;
     const tagify = new Tagify(addEventListener('input[name=tags]'));
@@ -35,6 +44,26 @@ export const test = (req, res) => {
     console.log('tagify', tagify);
     console.log('  tagify.addTags(inputText);', tagify.addTags(inputText));
     res.redirect('/');
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error });
+  }
+};
+
+export const tagconverter = async (req, res) => {
+  try {
+    console.log('data', req.body);
+    const data = {
+      name: 'ram',
+      age: 25,
+      gender: 'male',
+    };
+    const input = req.body.input;
+    const formattedMessage = input.replace(
+      /{(\w+)}/g,
+      (match, key) => data[key]
+    );
+    console.log('formattedMessage', formattedMessage);
+    res.render('field', { formattedMessage });
   } catch (error) {
     return res.status(500).send({ status: false, message: error });
   }
--- route/index.js
+++ route/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createContent, test } from '../controller/content.controller.js';
+import { createContent, tagconverter, test } from '../controller/content.controller.js';
 import tagify from '@yaireo/tagify';
 
 const router = express.Router();
 
 router.post('/content', createContent);
 router.post('/test', test);
+router.post('/tagconverter', tagconverter);
 
 export default router;
